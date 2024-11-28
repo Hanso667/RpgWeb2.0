7f05fb7d5c3b88868c3f6c8b931fa81efbf7df52
Hanso <168148485+Hanso667@users.noreply.github.com>
2024-11-27 21:30:11 -0300

    Add files via upload

--- src/scripts/Stats.js
+++ src/scripts/Stats.js
@@ -1,14 +1,51 @@
-var MaxHp,
-    Health,
-    MaxMp,
-    Mana;
-
-function setMaxHp(MaxHp){
-    this.Health=MaxHp;
-    this.MaxHp=MaxHp;
+function setMaxHp() {
+    var value = document.getElementById("MaxHp").value;
+    document.getElementById("Health").max =Math.floor(value);
+    document.getElementById("Health").value = Math.floor(value);
+    var max = document.getElementById("Health").max;
+    var current = document.getElementById("Health").value;
+    document.getElementById("display-health").textContent = "Health "+current+"/"+max;
 }
 
-function setMaxMp(MaxMp){
-    this.Mana=MaxMp;
-    this.MaxMp=MaxMp;
+function setMaxMp() {
+    var value = document.getElementById("MaxMp").value;
+    document.getElementById("Mana").max = Math.floor(value);
+    document.getElementById("Mana").value = Math.floor(value);
+    var max = document.getElementById("Mana").max;
+    var current = document.getElementById("Mana").value;
+    document.getElementById("display-mana").textContent = "Mana "+current+"/"+max;
+}
+function damage() {
+    var value = document.getElementById("Dano").value;
+    var Health = document.getElementById("Health").value;
+    var MaxHp = document.getElementById("Health").max;
+    if (Health - value >= 0 && Health - value <= MaxHp) {
+        Health -= value;
+    }
+     else if (Health - value >= 0 && Health - value > MaxHp) {
+        Health = MaxHp;
+    }
+     else if (Health - value < 0 && Health - value <= MaxHp) {
+        Health = 0;
+    }
+    document.getElementById("Health").value = Math.floor(Health);
+    document.getElementById("display-health").textContent = "Health "+Health+"/"+MaxHp;
+
+}
+function use() {
+    var value = document.getElementById("Uso").value;
+    var Mana = document.getElementById("Mana").value;
+    var MaxMp = document.getElementById("Mana").max;
+    if (Mana - value >= 0 && Mana - value <= MaxMp) {
+        Mana -= value;
+    }
+     else if (Mana - value > 0 && Mana - value > MaxMp) {
+        Mana = MaxMp;
+    }
+     else if (Mana - value < 0 && Mana - value <= MaxMp) {
+        Mana = 0;
+    }
+    document.getElementById("Mana").value = Math.floor(Mana);
+    document.getElementById("display-mana").textContent = "Mana "+Mana+"/"+MaxMp;
+
 }
